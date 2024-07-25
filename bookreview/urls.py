6fb5fb275a76edf1bff72c0d6c9324edc6a3661c
hvanbelle <hvanbelle@gmail.com>
2024-07-25 13:48:56 +0200

    Add books to index.html

--- bookreview/urls.py
+++ bookreview/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='author-list'),
+    path('', views.index_view, name='author-book-list'),
     path('authors/', views.AuthorView.as_view()),
     path('authors/<int:pk>', views.AuthorInstanceView.as_view(), name='author-instance'),
     path('books/', views.BookView.as_view()),
